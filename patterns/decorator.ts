84ab3e4d19d2af74a8b53160b614092538d7eca7
Thiện Đỗ <thiendo3831@gmail.com>
2022-10-10 23:18:55 +0700

    decorator

---
+++ patterns/decorator.ts
@@ -0,0 +1,45 @@
+interface Phone {
+  getCost(): number;
+}
+
+class SmartPhone implements Phone {
+  getCost() {
+    return 599;
+  }
+}
+
+abstract class PhoneDecorator implements Phone {
+  protected abstract cost: number;
+  constructor(private readonly inner: Phone) {}
+
+  getCost() {
+    return this.cost + this.inner.getCost();
+  }
+}
+
+class ChargeAdapter extends PhoneDecorator {
+  protected cost = 100;
+}
+
+class ChargeCable extends PhoneDecorator {
+  protected cost = 50;
+}
+
+class SkinWrap extends PhoneDecorator {
+  protected cost = 80;
+}
+
+class ScreenProtector extends PhoneDecorator {
+  protected cost = 120;
+}
+
+class InEarHeadphone extends PhoneDecorator {
+  protected cost = 45;
+}
+
+const smartPhone = new ChargeAdapter(
+  new ChargeCable(
+    new SkinWrap(new ScreenProtector(new InEarHeadphone(new SmartPhone())))
+  )
+);
+console.log(smartPhone.getCost());
--- patterns/singleton.ts
+++ patterns/singleton.ts
@@ -2,24 +2,24 @@ let instance: Counter;
 let singletonCount = 0;
 
 class Counter {
-    constructor() {
-        if (instance) {
-            throw new Error('You can only create one instance!');
-        }
-        instance = this;
+  constructor() {
+    if (instance) {
+      throw new Error("You can only create one instance!");
     }
+    instance = this;
+  }
 
-    getCount() {
-        return singletonCount;
-    }
+  getCount() {
+    return singletonCount;
+  }
 
-    increment() {
-      singletonCount++;
-    }
+  increment() {
+    singletonCount++;
+  }
 
-    decrement() {
-      singletonCount--;
-    }
+  decrement() {
+    singletonCount--;
+  }
 }
 
 const counter = new Counter();
