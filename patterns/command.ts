39c9a2c6aee4a9064961fbc23e66c31182867951
Thiện Đỗ <thiendo3831@gmail.com>
2022-10-26 16:28:39 +0700

    command

---
+++ patterns/command.ts
@@ -0,0 +1,65 @@
+interface Command {
+    execute(): void;
+    undo(): void;
+}
+
+interface Receiver {
+    turnOnAction(): void;
+    turnOffAction(): void;
+}
+
+class Remote {
+    constructor(private readonly turnOnCommand: Command, private readonly turnOffCommand: Command) { }
+
+    turnOnButtonClick() {
+        this.turnOnCommand.execute();
+    }
+
+    turnOffButtonClick() {
+        this.turnOffCommand.execute();
+    }
+}
+
+class AirCondition implements Receiver {
+    turnOnAction() {
+        console.log('Turn on!');
+    }
+
+    turnOffAction() {
+        console.log('tTurn off!');
+    }
+}
+
+class TurnOnCommand implements Command {
+    constructor(private readonly receiver: Receiver) { }
+
+    execute() {
+        this.receiver.turnOnAction();
+    }
+
+    undo() {
+        this.receiver.turnOffAction();
+    }
+}
+
+class TurnOffCommand implements Command {
+    constructor(private readonly receiver: Receiver) { }
+
+    execute() {
+        this.receiver.turnOffAction();
+    }
+
+    undo() {
+        this.receiver.turnOnAction();
+    }
+}
+
+const airCondition = new AirCondition();
+
+const turnOnCommand = new TurnOnCommand(airCondition);
+const turnOffCommand = new TurnOffCommand(airCondition);
+
+const remote = new Remote(turnOnCommand, turnOffCommand);
+
+remote.turnOnButtonClick();
+remote.turnOffButtonClick();
